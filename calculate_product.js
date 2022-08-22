c2aadedef042f1a8be498eddefb31e9bdedc925c
Amrita Tiwari <amrita20@navgurukul.org>
2022-08-22 13:58:01 +0530

    adding calculate product

---
+++ calculate_product.js
@@ -0,0 +1,6 @@
+// Calculate the product of two numbers, and return the result:
+
+function calculateProduct( product1 , product2){
+    return product1*product2 ;
+}
+console.log(calculateProduct(3,4));
